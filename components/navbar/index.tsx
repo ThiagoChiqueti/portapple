e97d7ef13d8cea4064ef708137086aae357c5b85
Thiago Chiqueti <tchiqueti@gmail.com>
2023-11-03 23:10:55 -0300

    desenvolvimento de portifólio inspirado na apple

---
+++ components/navbar/index.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import {FaInstagramSquare, FaLinkedin, FaWhatsappSquare} from 'react-icons/fa'
+const Navbar: React.FC = () => {
+  return (
+    <div className="w-full flex flex-row items-center justify-center fixed bottom-5">
+      <div className="max-w-lg flex flex-row flex-1 items-center justify-center gap-4 p-3 bg-neutral-800 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-600 ">
+        <div className=" hover:-translate-y-6 hover:scale-110 transition-all duration-200 cursor-pointer">
+          <Image src='/linkedin.svg' alt='linkedin' width={70} height={70} title='Linkedin' className='rounded'/>
+        </div>
+
+        <div className=" hover:-translate-y-6 hover:scale-110 transition-all duration-200 cursor-pointer">
+        <Image src='/whats.svg' alt='WhatsApp' width={70} height={70} title='Whatsapp' className='rounded'/>
+        </div>
+
+        <div className=" hover:-translate-y-6 hover:scale-110 transition-all duration-200 cursor-pointer">
+        <Image src='/instagram.svg' alt='Instagram' width={75} height={75} title='Instagram' className='rounded'/>
+        </div>
+
+        <div className=" hover:-translate-y-6 hover:scale-110 transition-all duration-200 cursor-pointer">
+        <Image src='/gmail.svg' alt='Gmail' width={70} height={70} title='Gmail' className='rounded'/>
+        </div>
+
+        <div className=" hover:-translate-y-6 hover:scale-110 transition-all duration-200 cursor-pointer">
+        <Image src='/github.svg' alt='GitHub' width={70} height={70} title='GitHub' className='rounded'/>
+        </div>
+        
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
