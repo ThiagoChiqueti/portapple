1a0418a204c9f138a295b957248753835ac2da69
Thiago Chiqueti <tchiqueti@gmail.com>
2023-11-18 12:16:23 -0300

    ajustes do scroll

--- components/stackSection/index.tsx
+++ components/stackSection/index.tsx
@@ -5,46 +5,16 @@ const StackSection: React.FC = () => {
   return (
     <Animation>
       <div className="flex flex-row flex-wrap items-center justify-center gap-5 sm:gap-10 mt-6">
-        <App_icon
-          src="/react.svg"
-          alt="React"
-          
-          name="React"
-        />
-        <App_icon
-          src="/python.svg"
-          alt="Python"
-         
-          name="Python"
-        />
+        <App_icon src="/react.svg" alt="React" name="React" />
+        <App_icon src="/python.svg" alt="Python" name="Python" />
 
         <a href="#elixir" rel="noopener noreferrer">
-          <App_icon
-            src="/elixir.svg"
-            alt="Elixir"
-          
-            name="Elixir"
-          />
+          <App_icon src="/elixir.svg" alt="Elixir" name="Elixir" />
         </a>
 
-        <App_icon
-          src="/node.svg"
-          alt="Node"
-          
-          name="NodeJs"
-        />
-        <App_icon
-          src="/postgres.svg"
-          alt="postgres"
-          
-          name="Postgres"
-        />
-        <App_icon
-          src="/docker.svg"
-          alt="Docker"
-         
-          name="Docker"
-        />
+        <App_icon src="/node.svg" alt="Node" name="NodeJs" />
+        <App_icon src="/postgres.svg" alt="postgres" name="Postgres" />
+        <App_icon src="/docker.svg" alt="Docker" name="Docker" />
       </div>
     </Animation>
   );
